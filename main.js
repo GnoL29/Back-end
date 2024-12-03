28a4549bbeb55f7b734ea929ac2f407963a2b82d
GnoL29 <nguyenduclong.cm2005@gmail.com>
2024-12-03 16:03:06 +0700

    Add index.html

---
+++ main.js
@@ -0,0 +1,10 @@
+// const port = 3000;
+// const http = require('http');
+
+// const sever = http.createServer((req, res) =>{
+//     res.statusCode=200; res.setHeader('Content-Type','text/plan');
+//     res.end('Hello')
+// })
+// sever.listen(port , ()=>{
+//     console.log('Sever is running');
+// })
